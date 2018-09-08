7d865fdbff1d5546565469255152597b7005722a
Azis Senoaji Prasetyotomo <azisseno@icloud.com>
2018-09-08 14:04:17 +0700

    initial commit project

---
+++ MovieGo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieGo
+//
+//  Created by Azis Senoaji Prasetyotomo on 08/09/18.
+//  Copyright © 2018 Azisseno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
