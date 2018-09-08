3fb49074f264ae30b0ae00ddca3aa3148d7ca476
Azis Senoaji Prasetyotomo <azisseno@icloud.com>
2018-09-08 18:25:36 +0700

    project setup & create api module

---
+++ Api/Api/Api.h
@@ -0,0 +1,19 @@
+//
+//  Api.h
+//  Api
+//
+//  Created by Azis Senoaji Prasetyotomo on 08/09/18.
+//  Copyright © 2018 Azisseno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Api.
+FOUNDATION_EXPORT double ApiVersionNumber;
+
+//! Project version string for Api.
+FOUNDATION_EXPORT const unsigned char ApiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Api/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,15 @@
+platform :ios, '9.0'
+use_frameworks!
+
+workspace 'MovieGo'
+
+pod 'Alamofire', '4.7.3'
+
+target 'MovieGo' do
+end
+
+target 'Api' do
+    project 'Api/Api.xcodeproj'
+end
+
+
