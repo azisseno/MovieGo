0e6a00ffb9077e509895ed4907cb7a68c4a0b31b
Azis Senoaji Prasetyotomo <azisseno@icloud.com>
2018-09-08 19:12:51 +0700

    add storage module and setup

---
+++ Storage/Storage/Storage.h
@@ -0,0 +1,19 @@
+//
+//  Storage.h
+//  Storage
+//
+//  Created by Azis Senoaji Prasetyotomo on 08/09/18.
+//  Copyright © 2018 Azisseno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Storage.
+FOUNDATION_EXPORT double StorageVersionNumber;
+
+//! Project version string for Storage.
+FOUNDATION_EXPORT const unsigned char StorageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Storage/PublicHeader.h>
+
+
