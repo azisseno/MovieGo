431250ded4ebf10e9558d2adf39ce9bb6ffb354f
Azis Senoaji Prasetyotomo <azisseno@icloud.com>
2018-09-10 11:57:27 +0700

    Nib Helper

---
+++ MovieGo/Core/Extensions/UIView.swift
@@ -0,0 +1,9 @@
+//
+//  UIView.swift
+//  MovieGo
+//
+//  Created by Azis Senoaji Prasetyotomo on 10/09/18.
+//  Copyright © 2018 Azisseno. All rights reserved.
+//
+
+import Foundation
---
+++ MovieGo/Modules/SearchMovies/Implementations/DataSources/MovieListDataSource.swift
@@ -0,0 +1,9 @@
+//
+//  MovieListDataSource.swift
+//  MovieGo
+//
+//  Created by Azis Senoaji Prasetyotomo on 10/09/18.
+//  Copyright © 2018 Azisseno. All rights reserved.
+//
+
+import Foundation
